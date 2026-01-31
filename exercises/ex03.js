de2ad1d129a208250ff4f7e0b13217fca6b8ef68
Pam Moll <109089506+pammyshark@users.noreply.github.com>
2026-01-30 23:08:48 -0800

    Update ex03.js
    Exercise done

--- exercises/ex03.js
+++ exercises/ex03.js
@@ -9,6 +9,16 @@ Create a function named numberOfVowels that will receive a string and return the
 
 const numberOfVowels = function (data) {
   // Put your solution here
+  let count = 0;
+  const vowels = "aeiou";
+
+  for (let i = 0; i < data.length; i++) {
+    if (vowels.includes(data[i])) {
+      count++;
+    }
+  }
+
+  return count;
 };
 
 console.log(numberOfVowels("orange")); // 3
