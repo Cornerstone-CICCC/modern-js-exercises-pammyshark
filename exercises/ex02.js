a825aed63627565b393ec3566c0ee5484882dc00
Pam Moll <109089506+pammyshark@users.noreply.github.com>
2026-01-30 23:06:12 -0800

    Update ex02.js
    Exercise done

--- exercises/ex02.js
+++ exercises/ex02.js
@@ -8,6 +8,16 @@ Create a function named conditionalSum that will be given an array of numbers an
 
 const conditionalSum = function (values, condition) {
   // Your code here
+  let sum = 0;
+
+  for (let i = 0; i < values.length; i++) {
+    if (condition === "even" && values[i] % 2 === 0) {
+      sum += values[i];
+    } else if (condition === "odd" && values[i] % 2 !== 0) {
+      sum = sum + values[i];
+    }
+  }
+  return sum;
 };
 
 console.log(conditionalSum([1, 2, 3, 4, 5], "even")); // 6
